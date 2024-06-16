2355aa8f02da7e6808f1ddb6440bb42ba92c61e4
John Hanley <jhanley708-git@s6.sector6.net>
2024-06-16 15:06:54 -0700

    Add savefig() helper

--- web_bench/client_speed_chart.py
+++ web_bench/client_speed_chart.py
@@ -8,6 +8,11 @@ import pandas as pd
 import seaborn as sns
 import typer
 from matplotlib import pyplot as plt
+from matplotlib import rcParams
+from matplotlib.axes import Axes
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.pyplot import figure
+from seaborn import FacetGrid
 
 
 def _get_rows(input_logfile: Path) -> Generator[dict[str, float], None, None]:
@@ -29,8 +34,21 @@ def speed_chart(input_logfile: Path = Path("/tmp/timings.txt")) -> None:
     csv = input_logfile.with_suffix(".csv")
     df.to_csv(csv, index=False)
 
-    sns.catplot(data=df)
-    plt.show()
+    def savefig(pdf: PdfPages, fig: Axes | FacetGrid) -> None:
+        assert fig
+        # print(figure().get_size_inches())
+        pdf.savefig()  # type: ignore [no-untyped-call]
+        plt.close()
+
+    with PdfPages(input_logfile.with_suffix(".pdf"), keep_empty=False) as pdf:  # type: ignore [no-untyped-call]
+        # sns.set_theme(rc={"figure.figsize": (3, 5)})
+        # rcParams["figure.figsize"] = 3, 5
+        # figure(figsize=(8, 6))
+        figsize = {"height": 3, "aspect": 1.3}
+        savefig(pdf, sns.boxplot(data=df))
+        savefig(pdf, sns.catplot(data=df))
+        savefig(pdf, sns.violinplot(data=df))
+        savefig(pdf, sns.kdeplot(data=df))
 
 
 if __name__ == "__main__":
