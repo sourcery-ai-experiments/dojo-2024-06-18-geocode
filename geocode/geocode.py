8b95446e2ab6c8e8bb4497bcacb58aad61048cc2
John Hanley <jhanley708-git@s6.sector6.net>
2024-06-18 18:15:53 -0700

    Write geocoded.csv

--- geocode/geocode.py
+++ geocode/geocode.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # Copyright 2024 John Hanley. MIT licensed.
-
+import csv
 from pathlib import Path
 
 import pandas as pd
@@ -11,26 +11,35 @@ assert tqdm
 data_dir = Path(__file__).parent / "data"
 
 
-def geocode(df: pd.DataFrame) -> pd.DataFrame:
+def geocode():
     """Adds lat, lon columns to df."""
-    geolocator = ArcGIS()
-    rows = [row for _, row in df.iterrows()]
-    for row in rows:
-        addr = f"{row.address}, {row.city}, {row.st} {row.zip}"
-        location = geolocator.geocode(addr)
-        if location:
-            row["lat"] = round(location.latitude, 5)
-            row["lon"] = round(location.longitude, 5)
-        else:
-            print(f"Could not geocode {addr}")
-        print("\n", row.lat, "\t", row.lon, "\t", addr)
-    return df
+
+    with open(data_dir / "geocoded.csv", "w") as fout:
+        fields = "address,city,st,zip,housenum,street,lat,lon"
+        sheet = csv.DictWriter(fout, fieldnames=fields.split(","))
+        sheet.writeheader()
+
+        geolocator = ArcGIS()
+        # rows = [row for _, row in df.iterrows()]
+        for _, row in pd.read_csv(data_dir / "resident_addr.csv").iterrows():
+            addr = f"{row.address}, {row.city}, {row.st} {row.zip}"
+            location = geolocator.geocode(addr)
+            if location:
+                row = dict(row)
+                row["lat"] = round(location.latitude, 5)
+                row["lon"] = round(location.longitude, 5)
+                sheet.writerow(row)
+                fout.flush()
+                print(end=".", flush=True)
+                print(dict(row))
+                yield dict(row)
+            else:
+                print(f"Could not geocode {addr}")
 
 
 def main() -> None:
-    df = pd.read_csv(data_dir / "resident_addr.csv")
-
-    df = geocode(df)
+    df = pd.DataFrame(geocode())
+    print(df)
     df.to_csv(data_dir / "geocoded.csv", index=False)
 
 
